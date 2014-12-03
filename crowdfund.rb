b8cf0755342f06128480fd9531a6eb4deeaf5e98
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-03 10:24:38 -0800

    bonus prag exercise coding a projects list

---
+++ crowdfund.rb
@@ -0,0 +1,8 @@
+project1 = "ABC"
+project2 = "LMN"
+project3 = "XYZ"
+funding1 = 1000
+projects = "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
+project1_funding = "Project #{project1} has $ #{funding1} in funding"
+
+puts "#{project1_funding}\n#{projects}"
