4d59ceda3fc9e25b16886815d6fd86fb20271534
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-03 10:16:07 -0800

    rewrote the file to start the actual game on excerize 3 of prag course

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,10 @@
-greeting = "Welcome!"
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
+players = "\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+health1 = 60
 
-3.times do 
-	puts greeting
-end
+puts "Players : #{players}"
 
-puts Time.now
+larry_current_health = puts "#{name1}'s health is #{health1}"
+
