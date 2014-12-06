32bd41c6d07a64b28bb2a5a0f105b278c93b25f2
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-05 16:07:40 -0800

    PS ex4 extra: added values to projects and tested various string method calls found with RI

--- crowdfund.rb
+++ crowdfund.rb
@@ -2,7 +2,12 @@ project1 = "ABC"
 project2 = "LMN"
 project3 = "XYZ"
 funding1 = 1000
+funding2 = 2586
+funding3 = 44225
 projects = "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
-project1_funding = "Project #{project1} has $ #{funding1} in funding"
 
-puts "#{project1_funding}\n#{projects}"
+
+puts "#{projects}
+Project #{project1.rjust(13,"*$%")} has $#{funding1} in funding
+Project #{project2.reverse.center(6)} has $#{funding2} in funding
+Project #{project3.replace "chickenpants"} has $#{funding3} in funding"
