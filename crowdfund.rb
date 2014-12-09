f58edb985ec0e11ee2836651b3b378ef389af16f
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-09 15:19:01 -0800

    PS extra ex 7: a fun one where i went a bit beyond the exercise by calculated the new distance from the goal.

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,20 +1,35 @@
-def project_status (project,funding=0)
-	"As of #{current_time}, #{project} has #{funding} in funding"
-end
+class Project
+	def initialize(name,intialfund,targetfund)
+		@name = name.capitalize
+		@intialfund = intialfund
+		@targetfund = targetfund
+		@distancefromgoal = targetfund - intialfund
+	end
+	
+	def to_s 
+	  "Project #{@name} has #{@intialfund} in funding toward a goal of #{@targetfund}."
+	end
+	
+	def addfunds(newfunds)
+		@newfunds = newfunds
+		@intialfund += newfunds
+		@distancefromgoal = @targetfund - @intialfund
+		puts "You've gained #{@newfunds}. Your total funds are now #{@intialfund}. You are #{@distancefromgoal} distance from goal."
 
-# %c is current date and timed in the .strftime method. %I:%M:%S is hour/min/sec, respectivly. 
-def current_time
-	right_now = Time.new
-	the_time = right_now.strftime("%c")
-end
+	end
 
-project1 = "ABC"
-project2 = "LMN"
-project3 = "XYZ"
-funding1 = 1000
-funding2 = 2586
-funding3 = 44225
+	def removefunds(removefunds)
+		@removefunds = removefunds
+		@intialfund -= removefunds
+		@distancefromgoal = @targetfund - @intialfund
+		puts "You've lost #{@removefunds}. Your total funds are now #{@intialfund}. You are #{@distancefromgoal} distance from goal."
+	end
 
-puts project_status(project1,funding1)
-puts project_status(project2,funding2)
-puts project_status(project3,funding3)
+		
+	end
+
+	project1=Project.new("billyball",200,5000)
+
+	puts project1
+	puts project1.removefunds(30)
+	puts project1.addfunds(4556)
