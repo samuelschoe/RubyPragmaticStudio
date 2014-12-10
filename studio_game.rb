8d8eab7dc7e8a6880894074f2bab40bcb8b5c6bc
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-10 14:16:12 -0800

    PS ex 8 - attribtutes and virtual attributes(methods)

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,19 @@
 class Player 
+
+	attr_reader :health
+	attr_accessor :name
+
 	def initialize (name, health=100)
 		@name = name.capitalize
 		@health = health
 	end
 
+	def score
+		@health + @name.length
+	end
+
 	def to_s
-		"I'm #{@name} with a health of #{@health}."
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
 	end
 
 	def blam
@@ -34,3 +42,11 @@ puts player3
 
 player3.w00ted
 puts player3
+
+puts player1.inspect
+puts player1.health
+puts player2.health
+puts player3.health
+
+player2.name="lawerence"
+puts player2.name
