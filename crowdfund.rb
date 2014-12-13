ea90e7d7f4a795199245fc9ef4ef23e8e1f412c7
anagramtogo <samuelschoe+bitbucket@gmail.com>
2014-12-12 20:41:27 -0800

    PS ex 9 extra - More array practice

--- crowdfund.rb
+++ crowdfund.rb
@@ -34,9 +34,40 @@ class Project
 	end
 
 	project1=Project.new("billyball",200,5000)
+	project2=Project.new("bopaboo",2003,34000)
+	project3=Project.new("kilopsa",2002,510200)
 
 	puts project1
 	puts project1.removefunds(30)
 	puts project1.addfunds(4556)
+
+    projects = [project1,project2,project3]
+
+    puts "We have #{projects.size} projects at the moment."
+
+    projects.each do |projectinfo|
+    	puts projectinfo
+    end
+
+
+    projects.each do |curentfund|
+    	puts curentfund.intialfund
+    end    
+
+
+    projects.each do |targetfund|
+    	puts targetfund.targetfund
+    end
+
+
+    projects.each do |x|
+    	puts x.addfunds(980)
+    end
+
+    projects.pop
+    project4 = Project.new("nipbip",345,21234)
+    projects.push(project4)
+    puts project4
+
 
     
