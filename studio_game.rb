2777de7ece2e0f31848f05ef17f1b5a3e52e042b
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-16 16:00:30 -0800

    PS ex 10 : using objects to interact with each other and arrays

--- studio_game.rb
+++ studio_game.rb
@@ -25,40 +25,47 @@ class Player
 		@health += 15
 		puts "#{@name} got w00ted!"
 	end
+end
+
+
+class Game
+
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_players(a_player)
+		@players << a_player
+	end
+
+	def play
+		puts "There are #{@players.size} players in #{@title}"
+		@players.each do |x| 
+			puts x
+		end
+
+		@players.each do |x|
+			x.blam
+			x.w00ted
+			puts x
+		end
+	end
 
 
 end
+
+
 
 player1 = Player.new("moe")
 player2 = Player.new("larry",60)
 player3 = Player.new("Curly",125)
 
-puts player1
-puts player2
-puts player3
 
-player3.blam
-puts player3
-
-player3.w00ted
-puts player3
-
-players = [player1,player2,player3]
-
-puts "There are #{players.size} players in the game"
-
-players.each do |x|
-	puts x
-end
-
-players.each do |x|
-	puts x.health
-end
-
-
-players.pop
-player4 = Player.new("Shemp", 90)
-players.push(player4)
-puts players
-
-
+Knuckleheads = Game.new("Knuckleheads")
+Knuckleheads.add_players(player1)
+Knuckleheads.add_players(player2)
+Knuckleheads.add_players(player3)
+Knuckleheads.play
