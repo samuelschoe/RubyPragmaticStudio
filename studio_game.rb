79764f1d0c5ba46ff77410b48e5f8432daf9aca8
Sam Schoeller <samuelschoe+bitbucket@gmail.com>
2014-12-16 16:05:23 -0800

    PS ex 10 : using objects to interact with each other and arrays : added in some chipmunks

--- studio_game.rb
+++ studio_game.rb
@@ -69,3 +69,17 @@ Knuckleheads.add_players(player1)
 Knuckleheads.add_players(player2)
 Knuckleheads.add_players(player3)
 Knuckleheads.play
+
+
+#############################################
+
+player4 = Player.new("Alvin", 80)
+player5 = Player.new("Simon", 125)
+player6 = Player.new("Theodore", 89)
+
+Chipmunks = Game.new("Chipmunks")
+Chipmunks.add_players(player4)
+Chipmunks.add_players(player5)
+Chipmunks.add_players(player6)
+Chipmunks.play
+
